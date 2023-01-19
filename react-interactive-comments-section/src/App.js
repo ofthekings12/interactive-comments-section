012f903906f2cab31bfa7caa67570355e658d411
Mikko De Los Reyes <mikkode212@gmail.com>
2023-01-19 01:16:18 -0500

    created AddComment component and imported into App.js.

--- react-interactive-comments-section/src/App.js
+++ react-interactive-comments-section/src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
+import AddComment from './Components/AddComment';
 
 function App() {
   return (
     <div className="App">
-      Yayo
+      <div className="comment-section">Multiple comments</div>
+      <AddComment/>
     </div>
   );
 }
---
+++ react-interactive-comments-section/src/Components/AddComment.js
@@ -0,0 +1,10 @@
+import './AddComment.scss'
+import React from 'react'
+
+function addComment() {
+  return (
+    <div className="add-comment">Add Comment</div>
+  )
+}
+
+export default addComment

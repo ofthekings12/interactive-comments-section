c67e9fbd8b7ddc2bec281cd1fd4d4a3f5c45218c
Mikko De Los Reyes <mikkode212@gmail.com>
2023-01-19 02:27:37 -0500

    styled add comment component. needs more styling. v sleepy....

--- react-interactive-comments-section/src/Components/AddComment.js
+++ react-interactive-comments-section/src/Components/AddComment.js
@@ -1,10 +1,17 @@
-import './AddComment.scss'
-import React from 'react'
+import "./AddComment.scss";
+import React from "react";
+import avatar from "../images/avatars/image-juliusomo.png";
 
 function addComment() {
   return (
-    <div className="add-comment">Add Comment</div>
-  )
+    <div className="add-comment">
+      <img className="avatar" src={avatar} alt="CurrentUser" />
+      <form className="form">
+       <textarea className="comment-field" type="text"  placeholder="Add a comment..."/>
+        <input className="submit-btn" type="submit" value="SEND"/>
+      </form>
+    </div>
+  );
 }
 
-export default addComment
+export default addComment;

b04af4c1814b0ae5f0e19124dac99561b039ab0c
Mikko De Los Reyes <mikkode212@gmail.com>
2023-01-19 19:01:38 -0500

    moved on from broken avatar image and started working on posted reply component

--- react-interactive-comments-section/src/Components/PostedReply.js
+++ react-interactive-comments-section/src/Components/PostedReply.js
@@ -1,31 +1,24 @@
-import React from "react";
-import "./Comment.scss";
-import Posts from "../data.json";
-import PostedReply from "./PostedReply";
+import React from 'react'
+import './PostedReply.scss'
 import avatar from "../images/avatars/image-amyrobson.png";
 
 
-export default function Comment() {
-  
+function PostedReply() {
   return (
 
-    <div>
-      {Posts &&
-        Posts.comments.map((post) => {
-          return (
-            <div className="comment" key={post.id}>
-              <div className="vote">
+    <div className="posted-reply" key="1">
+              <div className="p-r-vote">
                 <svg
-                  className="upvote"
+                  className="p-r-upvote"
                   width="11"
                   height="11"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z" />
                 </svg>
-                <div className="vote-count">{post.score}</div>
+                <div className="p-r-vote-count">Score</div>
                 <svg
-                  className="downvote"
+                  className="p-r-downvote"
                   width="11"
                   height="3"
                   xmlns="http://www.w3.org/2000/svg"
@@ -33,21 +26,21 @@ export default function Comment() {
                   <path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" />
                 </svg>
               </div>
-              <div className="comment-content-header">
-                <div className="comment-header">
-                  <div className="comment-details">
+              <div className="p-r-content-header">
+                <div className="p-r-header">
+                  <div className="p-r-details">
                     <img
-                      className="user-avatar"
-                      key={post.id}
+                      className="p-r-user-avatar"
+                      key="1"
                       src={avatar}
                       // alt="userAvatar"
                     />
-                    <div className="user-handle">{post.user.username}</div>
-                    <div className="created-at">{post.createdAt}</div>
+                    <div className="p-r-user-handle">username</div>
+                    <div className="p-r-created-at">createdAt</div>
                   </div>
-                  <div className="reply">
+                  <div className="p-r-reply">
                     <svg
-                      className="reply-icon"
+                      className="p-r-reply-icon"
                       width="14"
                       height="13"
                       xmlns="http://www.w3.org/2000/svg"
@@ -60,12 +53,10 @@ export default function Comment() {
                     <div>Reply</div>
                   </div>
                 </div>
-                <div className="comment-content">{post.content}</div>
+                <div className="p-r-content">Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!</div>
               </div>
             </div>
-          );
-        })}
-        <PostedReply/>
-    </div>
-  );
+  )
 }
+
+export default PostedReply

82f1e91c51ce82000d521108279e12f2646580d7
Mikko De Los Reyes <mikkode212@gmail.com>
2023-01-20 10:07:00 -0500

    conditional rendering of edit/delete buttons when post username matches current logged in user

--- react-interactive-comments-section/src/Components/PostedReply.js
+++ react-interactive-comments-section/src/Components/PostedReply.js
@@ -3,6 +3,8 @@ import "./PostedReply.scss";
 import Posts from "../data.json";
 
 function PostedReply() {
+  let currentUserUsername = Posts.currentUser.username;
+
   return (
     <div>
       {Posts &&
@@ -34,7 +36,38 @@ function PostedReply() {
                           </svg>
                         </div>
                         <div className="p-r-content-header">
+                          {currentUserUsername === reply.user.username ? 
+                          // beginning of logged in user
                           <div className="p-r-header">
+                            <div className="p-r-details">
+                              <img
+                                className="p-r-user-avatar"
+                                src={reply.user.image.png}
+                                alt="userAvatar"
+                              />
+                              <div className="p-r-user-handle" key={reply.id}>
+                                {reply.user.username}
+                              </div>
+                              <div className="p-r-created-at">
+                                {reply.createdAt}
+                              </div>
+                            </div>
+                            {/* edit & delete */}
+                            <div className="editDelete">
+                              <div className="delete">
+                              <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M1.167 12.448c0 .854.7 1.552 1.555 1.552h6.222c.856 0 1.556-.698 1.556-1.552V3.5H1.167v8.948Zm10.5-11.281H8.75L7.773 0h-3.88l-.976 1.167H0v1.166h11.667V1.167Z" fill="#ED6368"/></svg>
+                              Delete
+                              </div>
+                              <div className="edit">
+                              <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M13.479 2.872 11.08.474a1.75 1.75 0 0 0-2.327-.06L.879 8.287a1.75 1.75 0 0 0-.5 1.06l-.375 3.648a.875.875 0 0 0 .875.954h.078l3.65-.333c.399-.04.773-.216 1.058-.499l7.875-7.875a1.68 1.68 0 0 0-.061-2.371Zm-2.975 2.923L8.159 3.449 9.865 1.7l2.389 2.39-1.75 1.706Z" fill="#5357B6"/></svg>
+                              Edit
+                              </div>
+                            </div>
+
+                          </div>
+
+                          // end of logged in user
+                          : <div className="p-r-header">
                             <div className="p-r-details">
                               <img
                                 className="p-r-user-avatar"
@@ -63,7 +96,19 @@ function PostedReply() {
                               <div>Reply</div>
                             </div>
                           </div>
-                          <div className="p-r-content"><span className="reply-to">@{reply.replyingTo}</span> {reply.content}</div>
+}
+                          {/* ------ */}
+
+                          
+
+
+                              {/* ------- */}
+                          <div className="p-r-content">
+                            <span className="reply-to">
+                              @{reply.replyingTo}
+                            </span>{" "}
+                            {reply.content}
+                          </div>
                         </div>
                       </div>
                     </div>

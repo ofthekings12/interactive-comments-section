84019be3b8712a512c767b624d3ac7e45685ccce
Mikko De Los Reyes <mikkode212@gmail.com>
2023-01-22 13:21:47 -0500

    built most of the postedreplyform, few css touches remaining

--- react-interactive-comments-section/src/Components/PostedReplyForm.js
+++ react-interactive-comments-section/src/Components/PostedReplyForm.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import './PostedReplyForm.scss'
+import Avatar from '../data.json'
 
 function PostedReplyForm({ replyId }) {
+
+  const avatar = Avatar.currentUser.image.png;
+
   return (
-    <div key={replyId}>PostedReplyForm</div>
+    <div key={replyId}
+    className="prf-grey-line">
+      <div className="posted-reply-form">
+      <img className="avatar" src={avatar} alt="currentUser" />
+      <form className="prf-form">
+        <textarea className="prf-form-field" type="text" placeholder='Reply to this comment...'/>
+        <input className='prf-btn' type="submit" value="REPLY"/>
+      </form>
+
+
+      </div>
+    </div>
   )
 }
 

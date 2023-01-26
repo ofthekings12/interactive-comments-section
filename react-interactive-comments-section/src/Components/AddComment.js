5a7030d1a364afac282e7f517d6912f9bc17eed9
Mikko De Los Reyes <mikkode212@gmail.com>
2023-01-26 11:27:28 -0500

    begining on posting comments. comment field input shows up on browser console

--- react-interactive-comments-section/src/Components/AddComment.js
+++ react-interactive-comments-section/src/Components/AddComment.js
@@ -1,19 +1,25 @@
 import "./AddComment.scss";
-import React from "react";
+import { useState, useEffect, React } from "react";
 import User from"../data.json"
 
-function addComment() {
+function AddComment() {
   const avatar = User.currentUser.image.png;
+  const [comment, setComment] = useState('');
+
+  console.log(comment, 'here')
+
+  // Fetch data for JSON-Server
+  
 
   return (
     <div className="add-comment">
       <img className="avatar" src={avatar} alt="CurrentUser" />
       <form className="form">
-       <textarea className="comment-field" type="text"  placeholder="Add a comment..."/>
+       <textarea className="comment-field" type="text"  placeholder="Add a comment..." onChange={(e) => setComment(e.target.value)}/>
         <input className="submit-btn" type="submit" value="SEND"/>
       </form>
     </div>
   );
 }
 
-export default addComment;
+export default AddComment;

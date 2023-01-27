db433193c22e5d8696d221319f0ad6f796d1ca9c
Mikko De Los Reyes <mikkode212@gmail.com>
2023-01-27 12:12:48 -0500

    reverting to 0091a2d848697e9125326490960ee1f6a62a4309

--- react-interactive-comments-section/src/Components/AddComment.js
+++ react-interactive-comments-section/src/Components/AddComment.js
@@ -1,25 +1,19 @@
 import "./AddComment.scss";
-import { useState, useEffect, React } from "react";
+import React from "react";
 import User from"../data.json"
 
-function AddComment() {
+function addComment() {
   const avatar = User.currentUser.image.png;
-  const [comment, setComment] = useState('');
-
-  console.log(comment, 'here')
-
-  // Fetch data for JSON-Server
-  
 
   return (
     <div className="add-comment">
       <img className="avatar" src={avatar} alt="CurrentUser" />
       <form className="form">
-       <textarea className="comment-field" type="text"  placeholder="Add a comment..." onChange={(e) => setComment(e.target.value)}/>
+       <textarea className="comment-field" type="text"  placeholder="Add a comment..."/>
         <input className="submit-btn" type="submit" value="SEND"/>
       </form>
     </div>
   );
 }
 
-export default AddComment;
+export default addComment;

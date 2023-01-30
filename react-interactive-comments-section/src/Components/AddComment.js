04a30ad03ff33d002798ca22349b7c0a87b71447
Mikko De Los Reyes <mikkode212@gmail.com>
2023-01-30 09:49:59 -0500

    AddComment component now extracts currentUser image from json data and displays image in add comment form

--- react-interactive-comments-section/src/Components/AddComment.js
+++ react-interactive-comments-section/src/Components/AddComment.js
@@ -1,19 +1,38 @@
 import "./AddComment.scss";
-import React from "react";
-import User from"../data.json"
+import { useEffect, useState } from "react";
+import axios from 'axios'
+// import User from"../data.json"
 
-function addComment() {
-  const avatar = User.currentUser.image.png;
+function AddComment() {
+  // const avatar = User.currentUser.image.png;
+  const [image, setImage] = useState(null)
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/currentUser')
+    .then(res => {
+      setImage(res.data.image.png)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+  }, [])
+  
 
   return (
-    <div className="add-comment">
-      <img className="avatar" src={avatar} alt="CurrentUser" />
-      <form className="form">
-       <textarea className="comment-field" type="text"  placeholder="Add a comment..."/>
-        <input className="submit-btn" type="submit" value="SEND"/>
-      </form>
+    <div>
+      {image &&
+          <div className="add-comment">
+          <img className="avatar" src={image} alt="CurrentUser" />
+          <form className="form">
+           <textarea className="comment-field" type="text"  placeholder="Add a comment..."/>
+            <input className="submit-btn" type="submit" value="SEND"/>
+          </form>
+          </div>
+          }
+        
+
     </div>
   );
 }
 
-export default addComment;
+export default AddComment;

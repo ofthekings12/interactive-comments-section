d16b0bbf83b92e1f61cae8378583dfdb0f35ceba
Mikko De Los Reyes <mikkode212@gmail.com>
2023-01-30 16:12:47 -0500

    comments can now be posted, however they appear above the replies. need to make sure resplies only appear under their respective comment

--- react-interactive-comments-section/src/Components/AddComment.js
+++ react-interactive-comments-section/src/Components/AddComment.js
@@ -1,34 +1,66 @@
 import "./AddComment.scss";
 import { useEffect, useState } from "react";
-import axios from 'axios'
+import axios from "axios";
 
 function AddComment() {
-  const [image, setImage] = useState(null)
+
+  
+  // fetching currentUser avatar
+  const [image, setImage] = useState(null);
+  const [username, setUsername] = useState(null)
 
   useEffect(() => {
-    axios.get('http://localhost:3001/currentUser')
-    .then(res => {
-      setImage(res.data.image.png)
-    })
-    .catch(error => {
-      console.error(error)
-    })
-  }, [])
-  
+    axios
+      .get("http://localhost:3001/currentUser")
+      .then((res) => {
+        setImage(res.data.image.png);
+        setUsername(res.data.username)
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
+  //fetching data for JSON-Server
+  const [comment, setComment] = useState("");
+
+  const submitComment = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:3001/comments", {
+        id: null,
+        content: comment,
+        createdAt: 'now',
+        score: 0,
+        user: {
+          image: {
+            png: image
+          },
+          username: username,
+        },
+        replies: []
+      })
+      setComment("");
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  
   return (
     <div>
-      {image &&
-          <div className="add-comment">
-          <img className="avatar" src={image} alt="CurrentUser" />
-          <form className="form">
-           <textarea className="comment-field" type="text"  placeholder="Add a comment..."/>
-            <input className="submit-btn" type="submit" value="SEND"/>
-          </form>
-          </div>
-          }
-        
-
+      <div className="add-comment">
+        {image && <img className="avatar" src={image} alt="CurrentUser" />}
+        <form className="form" onSubmit={submitComment}>
+          <textarea
+            className="comment-field"
+            type="text"
+            placeholder="Add a comment..."
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+          <input className="submit-btn" type="submit" value="SEND" />
+        </form>
+      </div>
     </div>
   );
 }
--- react-interactive-comments-section/src/Components/Comment.js
+++ react-interactive-comments-section/src/Components/Comment.js
@@ -60,6 +60,7 @@ const toggleReply = (id) => {
                     <path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" />
                   </svg>
                 </div>
+                {/* <div className='comment-content-header-container'> */}
                 <div className="comment-content-header">
                   <div className="comment-header">
                     <div className="comment-details">
@@ -72,6 +73,9 @@ const toggleReply = (id) => {
                       <div className="user-handle">{comment.user.username}</div>
                       <div className="created-at">{comment.createdAt}</div>
                     </div>
+
+
+
                     <div
                       className="reply"
                       key={comment.id}
@@ -95,13 +99,16 @@ const toggleReply = (id) => {
                   </div>
                   <div className="comment-content">{comment.content}</div>
                 </div>
+                {/* </div> */}
               </div>
               { 
-              showReply === comment.id && (<ReplyForm commentId={comment.id} /> )}
+              showReply === comment.id && (
+              <ReplyForm commentId={comment.id}/>
+               )}
             </div>
           );
         })}
-      <PostedReply />
+        <PostedReply />
 
 
     </div>

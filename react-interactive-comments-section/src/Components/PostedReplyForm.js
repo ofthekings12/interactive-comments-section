644abeca68a1470799491d714fde2800faf02f34
Mikko De Los Reyes <mikkode212@gmail.com>
2023-02-08 15:19:53 -0500

    posted reply form now renders currentuser avatar using usestate/useeffect hooks to get data from backend

--- react-interactive-comments-section/src/Components/PostedReplyForm.js
+++ react-interactive-comments-section/src/Components/PostedReplyForm.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 import './PostedReplyForm.scss'
-import Avatar from '../data.json'
 
 function PostedReplyForm({ replyId }) {
 
-  const avatar = Avatar.currentUser.image.png;
+  // fetching currentUser avatar
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/currentUser")
+      .then((res) => {
+        setImage(res.data.image.png);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
   return (
     <div key={replyId}
     className="prf-grey-line">
       <div className="posted-reply-form">
-      <img className="avatar" src={avatar} alt="currentUser" />
+      <img className="avatar" src={image} alt="currentUser" />
       <form className="prf-form">
         <textarea className="prf-form-field" type="text" placeholder='Reply to this comment...'/>
         <input className='prf-btn' type="submit" value="REPLY"/>
